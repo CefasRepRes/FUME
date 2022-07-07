55007f94c02da056fffcb973064e05c016dbfeb5
Tiago Silva <tiago.silva@cefas.co.uk>
2022-07-07 18:01:09 +0100

    add setup.py to allow installation

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+setup(name='FUME',
+      version='1.0',
+      description='Forel Ule ',
+      url='https://github.com/jobel-openscience/FUME',
+      py_modules=['fume'],
+      )
